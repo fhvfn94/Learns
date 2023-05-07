69c853b023a16e0217d667a54aaf40e9f84f0e54
Eldar <fhvfn94@gmail.com>
2023-05-07 13:10:14 +0300

    first commit

---
+++ src/main/java/com/skyPro/Learns/LearnsApplication.java
@@ -0,0 +1,13 @@
+package com.skyPro.Learns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skyPro/Learns/LearnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyPro.Learns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
